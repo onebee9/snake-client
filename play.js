a872a8bd75545bfff7743927926bdb791b8e8473
ibifuro odu <oduibifuro@gmail.com>
2021-12-07 21:28:57 -0500

    getting user input to close the game

--- play.js
+++ play.js
@@ -1,19 +1,22 @@
  const connect = require("./client.js").connect;
  
-//  const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   return stdin;
-// };
+ const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data",(key)=> {handleUserInput(key)});
+  return stdin;
+};
 
-// stdin.on("data", handleUserInput);
 
-// const handleUserInput = function () {
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
-// };
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
 
 connect();
