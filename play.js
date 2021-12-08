08c55702b22991e44fcd5c3a52ff8c886fc04094
ibifuro odu <oduibifuro@gmail.com>
2021-12-07 22:28:33 -0500

    refactor the files to use

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
- const connect = require("./client.js").connect;
- 
- const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data",(key)=> {handleUserInput(key)});
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+console.log("Connecting ...");
+connect();
 
 setupInput();
-
-connect();
